03059dd0bcc55ec76921dbc5f509f457b0a462e2
PRATIKGRX <pratikgrx06@gmail.com>
2025-03-19 18:26:44 +0545

    updated styles
    link of Web
    https://pratikgrx.github.io/to-do-app/

--- script.js
+++ script.js
@@ -1,70 +1,28 @@
-var tasks = JSON.parse(localStorage.getItem("tasks"))??[];
-console.log(tasks);
-var globalIndex;
-displayOutput();
-function displayOutput()
-{
-    var tasksString     = "";
-    for (let i = 0; i < tasks.length; i++) {
-        tasksString = tasksString + `
-                    <tr>
-                        <td>`+(i+1)+`</td>
-                        <td>`+tasks[i]+`</td>
-                        <td>
-                            <button onclick=editTask(`+i+`)>Edit</button>
-                            <button onclick="deleteTask(`+i+`)">Delete</button>
-                        </td>
-                    </tr>
-        `;
+var tasks = ["task", "taslk1", "taslk2"];
+showTask();
+function showTask(){
+    var stringTask = "";
+    for(var i = 0; i < tasks.length; i++){
+        stringTask += ` <tr>
+                <td>`+(i+1)+`</td>
+                <td>`+tasks[i]+`</td>
+                <td><Button onclick="editTask(`+i+`)">Edit</Button>
+                <button>Delete</button></td>
+            </tr>`
     }
-    document.getElementById("task_body").innerHTML=tasksString;
+    document.getElementById("tableBody").innerHTML = stringTask;
 }
-
 function addTask(event){
     event.preventDefault();
-    var userVal = document.getElementById("add_text_input").value;
-    if(String(userVal).trim=""){
-        alert("Please enter the valid text:");
+    var userInput = document.getElementById("userInput").value;
+    if(String(userInput).trim() == ""){
+        alert("Please enter a task");
         return;
     }
-    document.getElementById("add_text_input").value="";
-    tasks.push(userVal);
-    displayOutput();
-    updateLocalStorage();
+    tasks.push(userInput);
+    document.getElementById("userInput").value = "";
+    showTask();
 }
-
-function deleteTask(index){
-    var confirmCheck=confirm("are you sure:");
-    if(confirmCheck){
-        tasks.splice(index,1);
-        displayOutput();
-        alert("delete successfully:")
-    }
-}
-function editTask(index){
-    document.getElementById("addForm").style.display="none";
-    document.getElementById("editForm").style.display="block";
-    var value=tasks[index];
-    document.getElementById("edit_text_input").value=value;
-    globalIndex=index;
-}
-
-
-function updateTask(event)
-{
-    event.preventDefault();//prevent to refresh the page
-    var userVal = document.getElementById("edit_text_input").value;
-    var index=globalIndex;
-    tasks[index]=userVal;
-    displayOutput();
-    updateLocalStorage();
-    document.getElementById("addForm").style.display="block";
-    document.getElementById("editForm").style.display="none";
-    alert("Task Edit Successfully")
-}
-
-
-function updateLocalStorage()
-{
-    localStorage.setItem("tasks",JSON.stringify(tasks));
+editTask(index){
+    
 }
