2d2563100a82ae35f614e1665d59567df8e28b73
PRATIKGRX <pratikgrx06@gmail.com>
2025-03-19 18:29:28 +0545

    style added
    https://pratikgrx.github.io/to-do-app/

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
-var tasks = ["task", "taslk1", "taslk2"];
+
+const tasks = JSON.parse(localStorage.getItem("tasks")) || ["task","task1","task2"];
+var globalIndex;
+const alertText = document.getElementById("alert");
+let editInput = document.getElementById("taskEdit");
+const userInput = document.getElementById("taskInput");
+userInput.addEventListener('focus',()=>{
+    alertText.style.display = 'none';
+});
+editInput.addEventListener('focus',()=>{
+    alertText.style.display = 'none';
+});
 showTask();
 function showTask(){
-    var stringTask = "";
-    for(var i = 0; i < tasks.length; i++){
-        stringTask += ` <tr>
-                <td>`+(i+1)+`</td>
-                <td>`+tasks[i]+`</td>
-                <td><Button onclick="editTask(`+i+`)">Edit</Button>
-                <button>Delete</button></td>
-            </tr>`
+    let taskString = "";
+    for(let i = 0; i<tasks.length; i++){
+        taskString += `<tr>
+                    <td class="pl-2"><input type="radio" name="" onclick="underlineText(`+i+`)"></td>
+                    <td class="pl-2" id="task-${i}">`+tasks[i]+`</td>
+                    <td class="w-[100px] flex justify-between p-[4px]">
+                        <button onclick="edit(`+i+`)" class="text-green-500 rounded-lg p-[4px] transition duration-500 hover:bg-green-500 hover:text-white">Edit</button>
+                        <button onclick="deleteTask(`+i+`)" class="text-red-500 rounded-lg  p-[2px] transition duration-500 hover:bg-red-500 hover:text-white">Delete</button>
+                    </td>
+                </tr>`;
+        
     }
-    document.getElementById("tableBody").innerHTML = stringTask;
+    document.getElementById("taskContainer").innerHTML = taskString;
 }
+
 function addTask(event){
+    let taskInput = document.getElementById("taskInput").value;
+    
     event.preventDefault();
-    var userInput = document.getElementById("userInput").value;
-    if(String(userInput).trim() == ""){
-        alert("Please enter a task");
+    if(String(taskInput.trim()) == ""){
+        
+        alertText.style.display = "block";
+        
         return;
     }
-    tasks.push(userInput);
-    document.getElementById("userInput").value = "";
+    
+    
+    console.log(taskInput);
+    tasks.push(taskInput);
+    localStorage.setItem("tasks", JSON.stringify(tasks)); 
+    showTask();
+    document.getElementById("taskInput").value = "";
+
+}
+function deleteTask(index){
+    tasks.splice(index,1);
+    localStorage.setItem("tasks", JSON.stringify(tasks)); 
     showTask();
 }
-editTask(index){
+function edit(index){
+    document.getElementById("add").style.display = "none";
+    document.getElementById("edit").style.display = "block"; 
+    document.getElementById("taskEdit").value= tasks[index];
+    globalIndex=index;
     
+}
+function updateTask(event){
+    event.preventDefault();
+    let newTaskName = document.getElementById("taskEdit").value;
+    if(String(newTaskName.trim()) == ""){
+        alertText.style.display = "block";
+        return;
+    }
+    tasks[globalIndex]=newTaskName;
+    localStorage.setItem("tasks", JSON.stringify(tasks)); 
+    showTask();
+    document.getElementById("add").style.display = "block";
+    document.getElementById("edit").style.display = "none"; 
 }
